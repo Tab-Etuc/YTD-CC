1df0b4af5300764cef1285345768255835c2c7e5
羅少希 <83118946+Tab-Etuc@users.noreply.github.com>
2022-06-02 15:57:56 +0800

    change ui

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import { createApp } from 'vue'
 import Notifications from '@kyvg/vue3-notification'
 import App from './App.vue'
+import { vfmPlugin } from 'vue-final-modal'
+import VDynamicModal from './components/VDynamicModal.vue'
+
 import './assets/tailwind.css'
 
 const app = createApp(App)
-app.use(Notifications)
+app.use(Notifications).use(vfmPlugin).component('VDynamicModal', VDynamicModal)
 
 app.mount('#app')
