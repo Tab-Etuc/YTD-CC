55bacd7480bb7bec2570c7a455a32dbc2dda2b71
羅少希 <83118946+Tab-Etuc@users.noreply.github.com>
2022-06-02 15:57:41 +0800

    change ui

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    class="
+    static
+    w-20 h-[90%]
+    mt-6
+    m-auto
+    flex p-4
+    bg-slate-700
+    rounded-xl 
+    shadow-2xl 
+    ring-1 ring-white/10 ring-inset
+    "
+  ></div
+></template>
+
+<script>
+export default {
+  name: 'Sidebar'
+}
+</script>
