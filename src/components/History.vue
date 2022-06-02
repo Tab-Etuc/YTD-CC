7f53a0837ab80047210f9f6d42d96f66d142aca4
羅少希 <83118946+Tab-Etuc@users.noreply.github.com>
2022-06-02 15:57:32 +0800

    change ui

---
+++ src/components/History.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    class="
+    relative bottom-0 mt-[1rem] w-full h-[25.2rem] m-auto "
+  >
+    <p
+      class="relative top-[1rem] mx-[1rem] tracking-wide cursor-defaultmt-2 text-lg leading-tight  select-none text-white"
+    >
+      Histroy
+    </p>
+    <hr class="static w-[calc(100%-2rem)] mx-auto h-0.5 bg-white mt-[1.5rem]"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'History'
+}
+</script>
