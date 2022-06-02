0e82b0a23d23c413ae2dfcf819d9e80553a51991
羅少希 <83118946+Tab-Etuc@users.noreply.github.com>
2022-06-02 15:57:35 +0800

    change ui

--- src/components/Mainpanel.vue
+++ src/components/Mainpanel.vue
@@ -1,77 +1,35 @@
 <template>
   <div
     class="
-      mt-16
-      place-items-center
-      my-auto
-      max-w-md
-      mx-auto
-      bg-gray-800
-      rounded-xl
-      shadow-2xl
-      overflow-hidden
-      md:max-w-3xl
-    "
+    static  m-auto mt-10 grid bg-slate-800 rounded-xl shadow-2xl ring-1 ring-white/10 ring-inset overflow-hidden w-[45rem]"
   >
     <div class="md:flex place-content-center p-4">
-      <div class="p-8">
+      <div class="p-6">
         <p
           class="
-            tracking-wide
-            cursor-default
-            mt-2
-            text-lg
-            leading-tight
-            font-medium
-            text-white text-center
-            select-none
-          "
+            tracking-wide cursor-defaultmt-2 text-lg leading-tight font-mediu text-white text-center select-none"
         >
           貼上 Youtube 影片連結
         </p>
-        <hr class="mt-3" size="8px" align="center" width="100%" />
+        <hr class="mt-3 w-full content-center h-2" />
         <div>
           <input
             v-model="url"
             class="
-              ml-2
-              mt-4
-              placeholder-gray-500
-              w-60
-              rounded-full
-              px-3
-              pl-5
-              py-1
-              transition-all
-              duration-700
-              ease-in-out
-              focus:shadow-outline
-              outline-indigo-400
-              hover:w-64
+              mx-2 mt-4 placeholder-gray-500 w-[28rem] rounded-full px-3 pl-5 py-1 transition-all duration-700 ease-in-out focus:shadow-outline outline-indigo-400 hover:w-[30rem]
             "
-            placeholder="https://www.youtube.com/..."
+            placeholder="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=..."
           />
           <button
             class="
-              mx-2
-              mt-3
-              text-center
-              bg-indigo-400
-              hover:bg-indigo-600
-              w-16
-              text-white
-              rounded-lg
-              focus:outline-none
-              focus:ring-2
-              focus:ring-indigo-600
-              focus:ring-opacity-50
-              p-0.5
+              mx-2 mt-3 text-center bg-blue-500 hover:bg-blue-600 w-16 text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-opacity-50 p-0.5
             "
             @click="btn"
           >
             確認
           </button>
         </div>
+        <v-button class="bg-blue-500 hover:bg-blue-600 w-16 text-white rounded-lg" @click="dynamic">Open Dynamic Modal</v-button>
         <!-- <select
             class="mx-2 bg-indigo-400 text-white rounded-lg w-16 p-1 "
             v-model="opt"
@@ -85,33 +43,40 @@
 </template>
 
 <script>
-import downloader from "../common/downloader";
+import downloader from '../common/downloader'
+
+import { $vfm } from 'vue-final-modal'
 
 export default {
-  name: "Mainpanel",
+  name: 'Mainpanel',
 
-  data() {
+  data () {
     return {
-      url: "",
-      opt: "",
-    };
+      url: '',
+      opt: ''
+    }
   },
 
-  inject: ["showNotify"],
+  inject: ['showNotify'],
 
   methods: {
     btn: async function () {
-      if (this.url == "")
-        return this.showNotify("foo-css", "請輸入連結！", "error");
+      if (this.url == '')
+        return this.showNotify('foo-css', '請輸入連結！', 'error')
 
       if (!/^www\.youtube.com|be/.test(this.url) || !/v=|.be\//.test(this.url))
-        return this.showNotify("foo-css", "連結無效！", "error");
-
-      downloader(this.url);
+        return this.showNotify('foo-css', '連結無效！', 'error')
+      downloader(this.url)
     },
+    dynamic () {
+      $vfm.show({
+        component: 'VDynamicModal'
+      })
+    }
   },
-  mounted() {
-    window.showNotify = this.showNotify;
-  },
-};
+
+  mounted () {
+    window.showNotify = this.showNotify
+  }
+}
 </script>
