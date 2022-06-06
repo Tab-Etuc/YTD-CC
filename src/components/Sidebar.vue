d01a2a4840bdff7958b64a1c7bb779dcbf26edab
羅少希 <83118946+Tab-Etuc@users.noreply.github.com>
2022-06-06 17:31:23 +0800

    add router

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,18 +1,9 @@
 <template>
   <div
     class="
-    static
-    w-20 h-[90%]
-    mt-6
-    m-auto
-    flex p-4
-    bg-slate-700
-    rounded-xl 
-    shadow-2xl 
-    ring-1 ring-white/10 ring-inset
-    "
-  ></div
-></template>
+    static w-20 h-[90%] mt-6 m-auto flex p-4 bg-slate-700 rounded-xl shadow-2xl ring-1 ring-white/10 ring-inset"
+  ></div>
+</template>
 
 <script>
 export default {
