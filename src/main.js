94e1e85d19866f281ea8497a7ecddbf0ffe26b06
羅少希 <83118946+Tab-Etuc@users.noreply.github.com>
2022-06-06 17:31:30 +0800

    add router

--- src/main.js
+++ src/main.js
@@ -1,12 +1,26 @@
 import { createApp } from 'vue'
 import Notifications from '@kyvg/vue3-notification'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
-import { vfmPlugin } from 'vue-final-modal'
-import VDynamicModal from './components/VDynamicModal.vue'
+import VueSidebarMenu from 'vue-sidebar-menu'
+import 'vue-sidebar-menu/dist/vue-sidebar-menu.css'
 
 import './assets/tailwind.css'
 
+import Home from './components/Home.vue'
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      component: Home
+    }
+  ]
+})
 const app = createApp(App)
-app.use(Notifications).use(vfmPlugin).component('VDynamicModal', VDynamicModal)
+app
+  .use(Notifications)
+  .use(VueSidebarMenu)
+  .use(router)
 
 app.mount('#app')
