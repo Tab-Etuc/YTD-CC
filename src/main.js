898845419b116006b08b36f1f69d1e2336e9da1c
羅少希 <83118946+Tab-Etuc@users.noreply.github.com>
2022-07-20 20:03:08 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,38 +1,38 @@
-import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
-import Notifications from '@kyvg/vue3-notification'
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
+import Notifications from "@kyvg/vue3-notification";
 
-import App from './App.vue'
+import App from "./App.vue";
 
-import './assets/tailwind.css'
+import "./assets/tailwind.css";
 
-import Home from './components/Home.vue'
-const About = { template: '' }
+import Home from "./components/Home.vue";
+const About = { template: "" };
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
-      path: '/',
-      component: Home
+      path: "/",
+      component: Home,
     },
     {
-      path: '/history',
-      component: About
+      path: "/history",
+      component: About,
     },
     {
-      path: '/about',
-      component: About
+      path: "/about",
+      component: About,
     },
     {
-      path: '/settings',
-      component: About
-    }
-  ]
-})
+      path: "/settings",
+      component: About,
+    },
+  ],
+});
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(Notifications).use(router)
+app.use(Notifications).use(router);
 
-app.mount('#app')
+app.mount("#app");
