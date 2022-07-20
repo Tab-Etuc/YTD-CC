ce9d4338a6dfa03ac926d46f2099201840a32744
羅少希 <83118946+Tab-Etuc@users.noreply.github.com>
2022-07-20 20:02:40 +0800

    Update main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,39 +6,37 @@
 mod ffmpeg;
 
 use anyhow::Result;
+use indicatif::{ProgressBar, ProgressStyle};
 use std::{
     fs,
     io::{self, copy, Read},
     path::{Path, PathBuf},
 };
 
-use indicatif::{ProgressBar, ProgressStyle};
 use url::Url;
-
-struct DownloadProgress<R> {
+struct DownloadProgress<'a, R> {
     inner: R,
-    progress_bar: ProgressBar,
+    progress_bar: &'a ProgressBar,
 }
 
-static mut PROGRESS_SIZE_NOW: usize = 0;
-static mut TOTAL_PROGRESS_SIZE: u64 = 0;
-
-impl<R: Read> Read for DownloadProgress<R> {
+impl<R: Read> Read for DownloadProgress<'_, R> {
     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
         self.inner.read(buf).map(|n| {
             self.progress_bar.inc(n as u64);
-            unsafe {
-                PROGRESS_SIZE_NOW += n;
-            };
+            unsafe { PROGRESS_SIZE_NOW += n };
             n
         })
     }
 }
 
+static mut PROGRESS_SIZE_NOW: usize = 0;
+static mut TOTAL_PROGRESS_SIZE: u64 = 0;
+
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
-            download_yt,
+            download_youtube,
+            merge,
             get_bar_total_size,
             get_bar_size_now
         ])
@@ -47,8 +45,9 @@ fn main() {
 }
 
 #[tauri::command]
-async fn download_yt(url: &str, filename: &str, onlyaudio: bool) -> Result<(), ()> {
+async fn download_youtube(url: &str, filename: &str, onlyaudio: bool) -> Result<(), ()> {
     let url = Url::parse(url).unwrap();
+    
     let resp = ureq::get(url.as_str()).call().unwrap();
     // Find the video size:
     let total_size = resp
@@ -58,63 +57,56 @@ async fn download_yt(url: &str, filename: &str, onlyaudio: bool) -> Result<(), (
         .unwrap();
     unsafe { TOTAL_PROGRESS_SIZE = total_size };
 
-    let mut request = ureq::get(url.as_str());
+    let request = ureq::get(url.as_str());
 
-    // Display a progress bar:
     let pb = ProgressBar::new(total_size);
-    pb.set_style(ProgressStyle::default_bar()
-               .template("{spinner:.green} [{elapsed_precise}] [{bar:40.green/blue}] {bytes}/{total_bytes} ({eta})")
-               .progress_chars("#>-"));
-
-    let file = Path::new(filename);
+    pb.set_style(ProgressStyle::default_bar().template("{spinner:.green} [{elapsed_precise}] [{bar:40.green/blue}] {bytes}/{total_bytes} ({eta})").progress_chars("#>-"));
+    let file = Path::new(&filename);
 
     if file.exists() {
-        // Continue the file:
-        let size = file.metadata().unwrap().len() - 1;
-        // Override the range:
-        request = ureq::get(url.as_str())
-            .set("Range", &format!("bytes={}-", size))
-            .to_owned();
-        pb.inc(size);
+        return Ok(());
     }
 
     let resp = request.call().unwrap();
     let mut source = DownloadProgress {
-        progress_bar: pb,
+        progress_bar: &pb,
         inner: resp.into_reader(),
     };
 
     let mut dest = fs::OpenOptions::new()
         .create(true)
         .append(true)
-        .open(&file)
-        .unwrap();
+        .open(&file).unwrap();
 
     let _ = copy(&mut source, &mut dest).unwrap();
-
-    if onlyaudio {
+    if onlyaudio{    
         let inpath = Path::new(&filename);
         let mut outpathbuf = PathBuf::from(&filename);
+
         outpathbuf.set_extension("mp3");
         let outpath = &outpathbuf.as_path();
 
         ffmpeg::to_audio(inpath, outpath);
-        // Get rid of the evidence.
-        fs::remove_file(&filename).unwrap();
 
-        // Success!
-        unsafe {
-            PROGRESS_SIZE_NOW = 0;
-            TOTAL_PROGRESS_SIZE = 0;
-        }
-        Ok(())
-    } else {
-        unsafe {
-            PROGRESS_SIZE_NOW = 0;
-            TOTAL_PROGRESS_SIZE = 0;
-        }
-        Ok(())
+        fs::remove_file(&filename).unwrap();
     }
+
+    unsafe {
+            PROGRESS_SIZE_NOW = 0;
+            TOTAL_PROGRESS_SIZE = 0;
+    }
+    Ok(())
+}
+
+#[tauri::command]
+async fn merge(videofile: String, audiofile: String, filename: String) {
+    let videopath = Path::new(&videofile);
+    let audiopath = Path::new(&audiofile);
+    let mut outpathbuf = PathBuf::from(&filename);
+    outpathbuf.set_extension("mp4");
+    let outpath = &outpathbuf.as_path();
+
+    ffmpeg::merge(videopath, audiopath, outpath);
 }
 
 #[tauri::command]
