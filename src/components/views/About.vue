f823bdc8aa4812410a862168b34fcaeb54996602
羅少希 <83118946+Tab-Etuc@users.noreply.github.com>
2022-08-10 20:03:33 +0800

    Create About.vue

---
+++ src/components/views/About.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="float-left mt-6 h-screen w-[calc(100%-7rem)] overflow-hidden">
+    <div class="relative float-left h-[90%] w-[22vw]">
+      <img src="../../assets/YTD.CC.png" class="mx-auto my-4 w-[60%]" />
+      <hr class="w-full" />
+      <a
+        class="h-4 w-8 bg-white"
+        @click="openLink('https://github.com/Tab-Etuc/YTD-CC')"
+        >Github 專案</a
+      >
+    </div>
+    <div class="absolute right-5 h-[90%] w-[calc(100%-10rem-22vw)]"></div>
+  </div>
+</template>
+
+<script>
+import { open } from "@tauri-apps/api/shell";
+
+export default {
+  name: "About",
+  components: {},
+
+  data() {
+    return {};
+  },
+
+  methods: {
+    async openLink(URL) {
+      await open(URL);
+    },
+  },
+};
+</script>
