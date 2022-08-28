a34f112acf3c055b7141bdbebc149da72c7ba267
羅少希 <83118946+Tab-Etuc@users.noreply.github.com>
2022-08-28 20:30:33 +0800

    Create Settings.vue

---
+++ src/components/views/Settings.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="mt-6 h-full w-[calc(100%-7rem)]">
+    <p
+      class="mb-10 flex w-full select-none justify-center text-4xl font-extrabold leading-tight tracking-wide text-blue-400"
+    >
+      設定
+    </p>
+    <div
+      class="m-auto mt-6 flex h-20 w-[70%] rounded-xl bg-slate-700 p-4 shadow-2xl ring-1 ring-inset ring-white/10"
+    ></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Settings',
+};
+</script>
