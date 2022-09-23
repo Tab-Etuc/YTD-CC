4d40ce181e36ea279c12a10786f082cac39fca1d
羅少希 <83118946+Tab-Etuc@users.noreply.github.com>
2022-09-23 22:09:56 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 // 自訂的分頁元件
 import Home from '../components/views/Home.vue';
+import history from '../components/views/history.vue';
 import About from '../components/views/About.vue';
 import Settings from '../components/views/Settings.vue';
-const temp = { template: '' };
 
 // 本文件需要匯出給Entry:main.js使用
 export default createRouter({
@@ -16,7 +16,7 @@ export default createRouter({
     },
     {
       path: '/history',
-      component: temp,
+      component: history,
     },
     {
       path: '/about',
