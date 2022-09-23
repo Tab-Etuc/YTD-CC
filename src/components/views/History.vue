8165392635a9af5c21396fbbc3760264eeb27c84
羅少希 <83118946+Tab-Etuc@users.noreply.github.com>
2022-09-23 22:08:57 +0800

    Create History.vue

---
+++ src/components/views/History.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="relative float-left h-screen w-[calc(100%-7rem)]">
+    <History />
+  </div>
+</template>
+
+<script>
+import History from '../History.vue';
+export default {
+  name: 'History',
+  components: { History },
+
+  data() {
+    return {};
+  },
+
+  methods: {},
+};
+</script>
